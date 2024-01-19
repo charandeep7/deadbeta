e9171c5af6da0880c87752a12c0d739c0d9fde0b
Abhishek Kumar <lit2021049@iiitl.ac.in>
2024-01-19 19:13:55 +0530

    Added sadcn

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import {Button} from '@/components/ui/button'
+
 export default function Home() {
   return (
     <main>
     <h1>This is our main page</h1>
+    <Button>Click Me</Button>
     </main>
   )
 }
