165eb70c22a53cdffcabcd12c9d4ef1e4fb4e29a
Abhishek Kumar <lit2021049@iiitl.ac.in>
2024-01-20 22:19:58 +0530

    Folder

--- app/components/landing/NavBar.jsx
+++ app/components/landing/NavBar.jsx
@@ -56,6 +56,7 @@ const Navbar = () => {
             className="flex h-9 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
             type="search"
             placeholder="Search"
+            width={50}
           ></input>
           <Button className="border border-gray-600 py-2 px-4 rounded focus:outline-none">
             <MagnifyingGlassIcon class="h-6 w-6 text-gray-500" />
@@ -101,7 +102,6 @@ const Navbar = () => {
                       >
                         Log IN
                       </Link>
-                      
                     }
                     {
                       <Link
