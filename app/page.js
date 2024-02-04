3b4dbdbbf02f30f76026df0d588b7e89823b3d97
Abhishek Kumar <lit2021049@iiitl.ac.in>
2024-02-04 21:25:50 +0530

    Committing local changes before merge

--- app/page.js
+++ app/page.js
@@ -4,8 +4,9 @@ import Footer from "./components/landing/Footer";
 
 export default function Home() {
   return (
-    <main className="bg-primaryBG dark:bg-secondaryBG min-h-screen">
+    <main className="bg-primaryBG dark:bg-secondaryBG dark:text-slate-400 min-h-screen">
       <HeroSection />
+      <Footer/>
     </main>
   );
 }
